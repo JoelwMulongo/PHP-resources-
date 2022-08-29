a96b8c572f07a05d53de47e034203a15f9754bdd
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-08-29 09:22:41 +0300

    Create Chmod.php

---
+++ Filesystem/Chmod.php
@@ -0,0 +1,21 @@
+
+// Changes file mode
+<?php
+chmod("/somedir/somefile", 755);   // decimal; probably incorrect
+chmod("/somedir/somefile", "u+rwx,go+rx"); // string; incorrect
+chmod("/somedir/somefile", 0755);  // octal; correct value of mode
+?>
+
+<?php
+// Read and write for owner, nothing for everybody else
+chmod("/somedir/somefile", 0600);
+
+// Read and write for owner, read for everybody else
+chmod("/somedir/somefile", 0644);
+
+// Everything for owner, read and execute for others
+chmod("/somedir/somefile", 0755);
+
+// Everything for owner, read and execute for owner's group
+chmod("/somedir/somefile", 0750);
+?>
