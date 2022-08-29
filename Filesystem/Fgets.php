916a631b45e995690162e25a0962d5f3dca6e497
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-08-29 09:43:52 +0300

    Create Fgets.php

---
+++ Filesystem/Fgets.php
@@ -0,0 +1,14 @@
+
+// Gets line from file pointer
+<?php
+$handle = @fopen("/tmp/inputfile.txt", "r");
+if ($handle) {
+    while (($buffer = fgets($handle, 4096)) !== false) {
+        echo $buffer;
+    }
+    if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail\n";
+    }
+    fclose($handle);
+}
+?>
