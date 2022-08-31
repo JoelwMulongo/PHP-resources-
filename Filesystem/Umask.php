2609c7285edb1af6a91a1cb7728ad87c36d4051b
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-08-31 06:25:20 +0300

    Create Umask.php

---
+++ Filesystem/Umask.php
@@ -0,0 +1,12 @@
+
+// Changes the current umask
+<?php
+$old = umask(0);
+chmod("/path/some_dir/some_file.txt", 0755);
+umask($old);
+
+// Checking
+if ($old != umask()) {
+    die('An error occurred while changing back the umask');
+}
+?>
