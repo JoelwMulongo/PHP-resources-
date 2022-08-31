83580328e6a71c624f58b4e402c755aa500240ff
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-08-31 06:40:30 +0300

    Create Rewind.php

---
+++ Filesystem/Rewind.php
@@ -0,0 +1,14 @@
+
+// Rewind the position of a file pointer
+<?php
+$handle = fopen('output.txt', 'r+');
+
+fwrite($handle, 'Really long sentence.');
+rewind($handle);
+fwrite($handle, 'Foo');
+rewind($handle);
+
+echo fread($handle, filesize('output.txt'));
+
+fclose($handle);
+?>
