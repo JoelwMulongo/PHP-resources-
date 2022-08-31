2b466a3fb4ee90f61c4e608b8b2980e8455289b1
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-08-31 06:49:31 +0300

    Create Popen.php

---
+++ Filesystem/Popen.php
@@ -0,0 +1,16 @@
+
+// Opens process file pointer
+<?php
+$handle = popen("/bin/ls", "r");
+?>
+
+<?php
+error_reporting(E_ALL);
+
+/* Add redirection so we can get stderr. */
+$handle = popen('/path/to/executable 2>&1', 'r');
+echo "'$handle'; " . gettype($handle) . "\n";
+$read = fread($handle, 2096);
+echo $read;
+pclose($handle);
+?>
