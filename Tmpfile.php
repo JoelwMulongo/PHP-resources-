84aaae983a63b53c29f1327a73b5d8cb40b3554c
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-08-31 06:28:20 +0300

    Create Tmpfile.php

---
+++ Tmpfile.php
@@ -0,0 +1,9 @@
+
+//Creates a temporary file
+<?php
+$temp = tmpfile();
+fwrite($temp, "writing to tempfile");
+fseek($temp, 0);
+echo fread($temp, 1024);
+fclose($temp); // this removes the file
+?>
