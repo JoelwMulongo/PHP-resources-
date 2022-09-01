767cc6aaf219a611e24fdfd5354f508f39a75cf0
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-09-01 07:20:10 +0300

    Create Fwrite.php

---
+++ Filesystem/Fwrite.php
@@ -0,0 +1,13 @@
+
+// Binary-safe file write
+<?php
+function fwrite_stream($fp, $string) {
+    for ($written = 0; $written < strlen($string); $written += $fwrite) {
+        $fwrite = fwrite($fp, substr($string, $written));
+        if ($fwrite === false) {
+            return $written;
+        }
+    }
+    return $written;
+}
+?>
