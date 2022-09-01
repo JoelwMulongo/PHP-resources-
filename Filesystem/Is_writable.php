92115015a0b05a56ec04ee51bb2f0ae5cc6e3f77
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-09-01 07:15:28 +0300

    Create Is_writable.php

---
+++ Filesystem/Is_writable.php
@@ -0,0 +1,10 @@
+
+// Tells whether the filename is writable
+<?php
+$filename = 'test.txt';
+if (is_writable($filename)) {
+    echo 'The file is writable';
+} else {
+    echo 'The file is not writable';
+}
+?>
