77c5b930d688d1faefd0ce064ac1a2d28539e060
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-09-01 07:31:07 +0300

    Create Filter input.php

---
+++ Filter/Filter input.php
@@ -0,0 +1,8 @@
+
+// Gets a specific external variable by name and optionally filters it
+<?php
+$search_html = filter_input(INPUT_GET, 'search', FILTER_SANITIZE_SPECIAL_CHARS);
+$search_url = filter_input(INPUT_GET, 'search', FILTER_SANITIZE_ENCODED);
+echo "You have searched for $search_html.\n";
+echo "<a href='?search=$search_url'>Search again.</a>";
+?>
