e353a96e481725b5e3132c663ee4a27ab806b9e0
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-09-03 01:47:13 +0300

    Create Array_combine.php

---
+++ Array/Array_combine.php
@@ -0,0 +1,9 @@
+
+// Creates an array by using one array for keys and another for its values
+<?php
+$a = array('green', 'red', 'yellow');
+$b = array('avocado', 'apple', 'banana');
+$c = array_combine($a, $b);
+
+print_r($c);
+?>
