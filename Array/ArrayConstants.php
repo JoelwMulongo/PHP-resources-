9cbf9f9a56775300a2f54eb43791dc2fd1747566
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-09-03 01:45:19 +0300

    Create ArrayConstants.php

---
+++ Array/ArrayConstants.php
@@ -0,0 +1,28 @@
+
+
+CASE_LOWER
+Used with array_change_key_case() to convert array keys to lower case
+CASE_UPPER
+Used with array_change_key_case() to convert array keys to upper case
+SORT_ASC
+Used with array_multisort() to sort in ascending order
+SORT_DESC
+Used with array_multisort() to sort in descending order
+SORT_REGULAR
+Used to compare items normally
+SORT_NUMERIC
+Used to compare items numerically
+SORT_STRING
+Used to compare items as strings
+SORT_LOCALE_STRING
+Used to compare items as strings, based on the current locale
+COUNT_NORMAL
+COUNT_RECURSIVE
+EXTR_OVERWRITE
+EXTR_SKIP
+EXTR_PREFIX_SAME
+EXTR_PREFIX_ALL
+EXTR_PREFIX_INVALID
+EXTR_PREFIX_IF_EXISTS
+EXTR_IF_EXISTS
+EXTR_REFS
