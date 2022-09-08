4c162616d0eb212880f5eb09faa76f2148a79767
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-09-08 09:07:01 +0300

    Create DebugBacktrace.php

---
+++ Error/DebugBacktrace.php
@@ -0,0 +1,18 @@
+
+// Generates a backtrace
+<?php
+// filename: /tmp/a.php
+
+function a_test($str)
+{
+    echo "\nHi: $str";
+    var_dump(debug_backtrace());
+}
+
+a_test('friend');
+?>
+
+<?php
+// filename: /tmp/b.php
+include_once '/tmp/a.php';
+?>
