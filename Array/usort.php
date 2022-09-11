39fc0b572fcf52cc90b3c43b95706667e9cfd910
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-09-11 21:16:29 +0300

    Create usort.php

---
+++ Array/usort.php
@@ -0,0 +1,19 @@
+
+// Sort an array by values using a user-defined comparison function
+<?php
+function cmp($a, $b)
+{
+    if ($a == $b) {
+        return 0;
+    }
+    return ($a < $b) ? -1 : 1;
+}
+
+$a = array(3, 2, 5, 6, 1);
+
+usort($a, "cmp");
+
+foreach ($a as $key => $value) {
+    echo "$key: $value\n";
+}
+?>
